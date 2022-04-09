05c0bce83edd7fc8238417446c320cb8410ab404
pqx <l.r.slegt@gmail.com>
2022-04-09 16:55:06 +0200

    jsx style applied

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
 const App = () => {
+	const buttonText = ['hello', 'world'];
+
 	return(
-		<div>Hi there</div>
+		<div>
+  			<label for="name" className="Label" >enter e-mail</label>
+  			<input id="name" type="text" />
+  			<button style={{ backgroundColor: 'red', color: 'white'}} >{ buttonText }</button>
+		</div>
 	)
 }
 
