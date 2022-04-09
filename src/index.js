eec4644691eb90ca022d7282e7f226110cb59fbc
Leo Slegt <86182182+pqxglobal@users.noreply.github.com>
2022-04-09 18:29:40 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-	const buttonText = {text: 'submitt'};
+	const buttonText = {text: 'submit'};
 
 	return(
 		<div>
